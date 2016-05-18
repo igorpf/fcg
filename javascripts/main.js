3739510586bd09c36d868aafac18b81c185c3335
joner1992 <filipejoner@gmail.com>
2016-05-18 00:12:09 -0300

    Three.js, Jquery e Bootstrap integrados ao projeto

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,4 +1,27 @@
+$(function(){
+	var scene = new THREE.Scene();
+	var camera = new THREE.PerspectiveCamera(45, 800/600, .1, 500);
+	var renderer= new THREE.WebGLRenderer();
+	renderer.setClearColor(0x000000);
+	renderer.setSize(800, 600);
 
-window.onload = function(){
-	webGLStart();
-};
+	var axis = new THREE.AxisHelper(10);
+	scene.add(axis);
+	var cubeGeometry = new THREE.BoxGeometry(5,5,5);
+	var cubeMaterials = new THREE.MeshBasicMaterial({color:0xdddddd, wireframe: true});
+	var cube = new THREE.Mesh( cubeGeometry, cubeMaterials);
+	cube.position.x = 0;
+	cube.position.y = 0;
+	cube.position.z = 0;
+
+	scene.add(cube);
+	camera.position.x = 40;
+	camera.position.y = 40;
+	camera.position.z = 40;
+
+	camera.lookAt(scene.position);
+
+	$('#scene-container').append(renderer.domElement);
+	renderer.render(scene, camera);
+});
+
