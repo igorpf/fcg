322cbc5abd4cb362f0e5abcea08dd312915a0aad
Igor Ferreira <ipferreira@inf.ufrgs.br>
2016-06-21 22:56:04 -0300

    Coloquei 3 câmeras e adicionei botões pra trocar entre elas

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,28 +1,70 @@
-$(function(){
-	var scene = new THREE.Scene();
-	var camera = new THREE.PerspectiveCamera(45, 800/600, .1, 500);//(viewangle, aspect, near, far)
-	var renderer= new THREE.WebGLRenderer();
-	renderer.setClearColor(0x000000);
-	renderer.setSize(800, 600);
+//globais
+var activeCamera = 1;
+var scene = new THREE.Scene();
+var perspCamera = new THREE.PerspectiveCamera(45, 800 / 600, .1, 500);//(viewangle, aspect, near, far)
+var chaseCamera = new THREE.PerspectiveCamera(45, 800 / 600, .1, 500);//(viewangle, aspect, near, far)
+var ortoCamera = new THREE.OrthographicCamera(-50, 50, 50, -50, 1, 500);
+var renderer = new THREE.WebGLRenderer();
+var axis = new THREE.AxisHelper(10);
+var cubeGeometry = new THREE.BoxGeometry(5, 5, 5);
+var cubeMaterials = new THREE.MeshBasicMaterial({color: 0xdddddd, wireframe: false});
+var cube = new THREE.Mesh(cubeGeometry, cubeMaterials);
+var keyboard = new THREEx.KeyboardState();
 
-	var axis = new THREE.AxisHelper(10);
-	scene.add(axis);
-        
-//	var cubeGeometry = new THREE.BoxGeometry(5,5,5);
-	var cubeGeometry = new THREE.CircleGeometry(5,32);
-	var cubeMaterials = new THREE.MeshBasicMaterial({color:0xdddddd, wireframe: true});
-	var cube = new THREE.Mesh( cubeGeometry, cubeMaterials);
-	cube.position.x = 0;
-	cube.position.y = 0;
-	cube.position.z = 0;
+init();
+animate();
+function init() {
+    scene.add(axis);
+    renderer.setClearColor(0x000000);
+    renderer.setSize(800, 600);
+    cube.position.x = 0;
+    cube.position.y = 0;
+    cube.position.z = 0;
+    scene.add(cube);
+    perspCamera.position.x = 40;
+    perspCamera.position.y = 40;
+    perspCamera.position.z = 40;
+    ortoCamera.position.x = 0;
+    ortoCamera.position.y = 0;
+    ortoCamera.position.z = 40;
+    //TODO: fazer a camera se mexer conforme o boneco mexe
+    chaseCamera.position.x = 0;
+    chaseCamera.position.y = 0;
+    chaseCamera.position.z = 40;
 
-	scene.add(cube);
-	camera.position.x = 0;
-	camera.position.y = 0;
-	camera.position.z = 40;
-        
-	camera.lookAt(scene.position);
-	$('#scene-container').append(renderer.domElement);
-	renderer.render(scene, camera);
-});
+    perspCamera.lookAt(scene.position);orto
+    $('#scene-container').append(renderer.domElement);
+    renderer.render(scene, perspCamera);
+}
 
+function animate() {
+    requestAnimationFrame(animate);
+    render();
+    update();
+}
+function update() {
+    if (keyboard.pressed("1")) {
+        activeCamera = 1;
+    }
+    if (keyboard.pressed("2")) {
+        activeCamera = 2;
+    }
+    if (keyboard.pressed("3")) {
+        activeCamera = 3;
+    }
+}
+function render() {
+    switch (activeCamera) {
+        case 1:
+            renderer.render(scene, perspCamera);
+            break;
+        case 2:
+            renderer.render(scene, ortoCamera);
+            break;
+        case 3:
+            renderer.render(scene, chaseCamera);
+            break;
+    }
+
+
+}
