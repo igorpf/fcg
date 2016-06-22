faea0a48b3216887771e331fabbbc6f95cdd11d2
joner1992 <filipejoner@gmail.com>
2016-06-22 00:33:35 -0300

    OBJ moving

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,9 +1,12 @@
+var stats;
+var camera, scene, renderer;
+var clock = new THREE.Clock();
+var keyboard = new THREEx.KeyboardState();
+var player, controls;
 function mainLoop() {
 	var container = $('#scene-container');
-	var stats;
-	var camera, scene, renderer;
 
-	var mouseX = 0, mouseY = 0;
+
 	var windowHalfX = container.width() / 2;
 	var windowHalfY = container.height() / 2;
 
@@ -20,43 +23,85 @@ function init() {
 	windowWidth = container.width();
 	windowHeight = container.height();
 
-	camera = new THREE.PerspectiveCamera( 45, windowWidth / windowHeight, 1, 2000 );
+	camera = new THREE.PerspectiveCamera(45, windowWidth / windowHeight, 1, 2000);
 	camera.position.z = 250;
 
-	// scene
+	//STATS
+	stats = new Stats();
+	container.append(stats.dom);
 
+
+	// scene
 	scene = new THREE.Scene();
 
-	var ambient = new THREE.AmbientLight( 0x444444 );
-	scene.add( ambient );
+	var ambient = new THREE.AmbientLight(0x444444);
+	scene.add(ambient);
 
-	var directionalLight = new THREE.DirectionalLight( 0xffeedd );
-	directionalLight.position.set( 0, 0, 1 ).normalize();
-	scene.add( directionalLight );
+	var directionalLight = new THREE.DirectionalLight(0xffeedd);
+	directionalLight.position.set(0, 0, 1).normalize();
+	scene.add(directionalLight);
+
+	this.player = new Player();
+	player.init();
 
 
 	renderer = new THREE.WebGLRenderer();
-	renderer.setPixelRatio( window.devicePixelRatio );
-	renderer.setSize( windowWidth, windowHeight );
-	container.append( renderer.domElement );
+	renderer.setPixelRatio(window.devicePixelRatio);
+	renderer.setSize(windowWidth, windowHeight);
+	controls = new THREE.OrbitControls( camera, renderer.domElement );
+	container.append(renderer.domElement);
 
 }
+
 
 //
 
 function animate() {
 
-	requestAnimationFrame( animate );
+	requestAnimationFrame(animate);
 	render();
+	update();
+	
 
+}
+
+function update()
+{
+	var delta = clock.getDelta(); // seconds.
+	var moveDistance = 200 * delta; // 200 pixels per second
+	var rotateAngle = Math.PI / 2 * delta;   // pi/2 radians (90 degrees) per second
+	
+	// local coordinates
+
+	// local transformations
+
+	// move forwards/backwards/left/right
+	if ( keyboard.pressed("W") )
+		player.player_object.translateZ( -moveDistance );
+	if ( keyboard.pressed("S") )
+		player.player_object.translateZ(  moveDistance );
+	if ( keyboard.pressed("A") )
+		player.player_object.translateX( -moveDistance );
+	if ( keyboard.pressed("D") )
+		player.player_object.translateX(  moveDistance );	
+
+	
+		
+	controls.update();
+	stats.update();
 }
 
 function render() {
-	camera.lookAt( scene.position );
+	camera.lookAt(scene.position);
 
-	renderer.render( scene, camera );
+	renderer.render(scene, camera);
 
 }
+
+
+
+
+
 
 
 
--- javascripts/models.js
+++ javascripts/models.js
@@ -1,57 +1,110 @@
-	//Arquivo responsável por carregar os modelos.
-
-	 // texture
-
-	var manager = new THREE.LoadingManager();
-	manager.onProgress = function(item, loaded, total) {
-
-	    console.log(item, loaded, total);
-
-	};
-
+//Arquivo responsável por carregar os modelos.
+var Player = function() {
+	var that = this;
+	var player_loader;
+	var player_object;
 	var texture = new THREE.Texture();
+	var posx = 0;
+	var posy = 0;
+	var posz = 0;
+
+	var direction = {
+		looking_left: false,
+		looking_right: false,
+		looking_top: false,
+		looking_down: true
+	}
 
 
-	var onProgress = function(xhr) {
-	    if (xhr.lengthComputable) {
-	        var percentComplete = xhr.loaded / xhr.total * 100;
-	        console.log(Math.round(percentComplete, 2) + '% downloaded');
-	    }
+	that.init = function() {
+
+		var manager = new THREE.LoadingManager();
+		manager.onProgress = function(item, loaded, total) {
+
+			console.log(item, loaded, total);
+
+		};
+
+		var onProgress = function(xhr) {
+			if (xhr.lengthComputable) {
+				var percentComplete = xhr.loaded / xhr.total * 100;
+				console.log(Math.round(percentComplete, 2) + '% downloaded');
+			}
+		};
+
+		var onError = function(xhr) {};
+
+		var texture_loader = new THREE.ImageLoader(manager);
+		texture_loader.load('obj/lego/face_Nikita.jpg', function(image) {
+
+			texture.image = image;
+			texture.needsUpdate = true;
+
+		});
+
+		// model
+		
+		var loader = new THREE.OBJLoader(manager);
+
+		loader.load('obj/be.obj', addObjtoScene);
+
+
 	};
 
-	var onError = function(xhr) {};
+	function addObjtoScene(object) {
 
-	var loader = new THREE.ImageLoader(manager);
-	loader.load('obj/lego/face_Nikita.jpg', function(image) {
+		object.traverse(function(child) {
+			if (child instanceof THREE.Mesh) {
 
-	    texture.image = image;
-	    texture.needsUpdate = true;
+				child.material.map = texture;
 
-	});
+			}
+		});
 
-	 // model
-	var loader = new THREE.OBJLoader(manager);
-	loader.load('obj/lego/be.obj', function(object) {
 
-	    object.traverse(function(child) {
+		object.position.x = posx;
+		object.position.y = posy;
+		object.scale.x = 0.2;
+		object.scale.y = 0.2;
+		object.scale.z = 0.2;
+		object.rotation.y = Math.PI;
+		object.rotation.x = 0;
 
-	        if (child instanceof THREE.Mesh) {
+		that.player_object = object;
 
-	            child.material.map = texture;
+		scene.add(that.player_object);
+	};
 
-	        }
 
-	    });
 
-	    object.position.x = 0;
-	    object.position.y = 0;
-	    object.scale.x = 0.2;
-	    object.scale.y = 0.2;
-	    object.scale.z = 0.2;
-	    object.rotation.y = -15;
-	    object.rotation.x = 0;
-	    scene.add(object);
-
-	}, onProgress, onError);
+	//Seters
+	that.setPosx = function(new_posx) {
+		that.posx = new_posx;
+	};
+	that.setPosy = function(new_posy) {
+		that.posy = new_posy;
+	};
+	that.setPosz = function(new_posz) {
+		that.posz = new_posz;
+	};
 
 	
+
+	//getters
+	that.getPosx = function() {
+		return that.posx;
+	};
+	that.getPosy = function() {
+		return that.posy;
+	};
+	that.getPosz = function() {
+		return that.posz;
+	};
+
+	that.getPlayerLoader = function() {
+		return that.player_loader;
+	};
+
+
+
+}
