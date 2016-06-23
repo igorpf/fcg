9828216ba74a114bc7778ff10f24dd2fcc78177d
Igor Ferreira <ipferreira@inf.ufrgs.br>
2016-06-22 23:09:12 -0300

    Carregando objetos da camada superior

--- javascripts/main.js
+++ javascripts/main.js
@@ -8,11 +8,35 @@ var keyboard = new THREEx.KeyboardState();
 var axis = new THREE.AxisHelper(10);
 var scene, renderer;
 var player, controls;
-var floor; //matrix
+var floor; //guarda a geometria/textura do chão
 var floorGeometry;
 var grassMaterial, grassTexture, waterMaterial, waterTexture;
+var numberToColorDict = {};
+var numberToType = {};
+var mapInferior;// guardam só os números
+var mapSuperior;// que representam os objetos
+var mapObjects;
+var mapScale = 20;
+var inputElement = document.getElementById("input1");
+var inputElement2 = document.getElementById("input2");
+var size = 20;
+/*initalization (THIS MUST BE MADE GLOBALLY)*/
+mapInferior = new Array(size);
+mapSuperior = new Array(size);
+floor = new Array(size);
+mapObjects = new Array(size);
+for (var i = 0; i < size; ++i) {
+    mapInferior[i] = new Array(size);
+    mapSuperior[i] = new Array(size);
+    floor[i] = new Array(size);
+    mapObjects[i] = new Array(size);
+}
+//quando escolher o arquivo, já carrega o mapa do jogo
+
+var src;
 
 var container = $('#scene-container');
+
 
 function mainLoop() {
 
@@ -32,8 +56,8 @@ function init() {
     chaseCamera.position.x = 0;
     chaseCamera.position.y = 0;
     chaseCamera.position.z = 40;
-    ortoCamera.up = new THREE.Vector3(0,0,-1);
-    ortoCamera.lookAt(new THREE.Vector3(0,-1,0));
+    ortoCamera.up = new THREE.Vector3(0, 0, -1);
+    ortoCamera.lookAt(new THREE.Vector3(0, -1, 0));
     //STATS
     stats = new Stats();
     container.append(stats.dom);
@@ -51,8 +75,8 @@ function init() {
     scene.add(directionalLight);
 
     this.player = new Player();
-    player.init();
-    
+    player.init(0, 5, 0);
+
     renderer = new THREE.WebGLRenderer();
     renderer.setClearColor(0x000000);
     renderer.setSize(800, 600);
@@ -106,7 +130,7 @@ function update()
     if (keyboard.pressed("3")) {
         activeCamera = 3;
     }
-    var relativeCameraOffset = new THREE.Vector3(0,300,600);
+    var relativeCameraOffset = new THREE.Vector3(0, 300, 600);
 
     var cameraOffset = relativeCameraOffset.applyMatrix4(player.player_object.matrixWorld);
 //    var cameraOffset = relativeCameraOffset.add(player.player_object.position);
@@ -117,7 +141,7 @@ function update()
     chaseCamera.lookAt(player.player_object.position);
 
 
-
+//console.log(mapObjects[5][17].block_object );
     controls.update();
     stats.update();
 }
@@ -141,5 +165,100 @@ function animate() {
     render();
     update();
 }
+/*----------------------MAP LOADING BEGIN-------------------------------------*/
+inputElement.addEventListener("change", handleFiles, false);
+inputElement2.addEventListener("change", handleFiles, false);
+inputElement2.disabled = true;
+function handleFiles(e) {
+    inputElement2.disabled = false;
+    inputElement.disabled = true;
+    src = e.srcElement.id;
+    var file = e.target.files[0];
+    var reader = new FileReader();
+    reader.addEventListener("load",
+            processimage, false);
+    reader.readAsArrayBuffer(file);
+}
+function processimage(e) {
+    var buffer = e.target.result;
+    var bitmap = getBMP(buffer);
+    var size = 20;
+    initializeDictionaries();
+    loadFloor();
+    for (var i = 0; i < size; ++i)
+        for (var j = 0; j < size; ++j) {
+            var index = 3 * (i * size + j);
+            var color = new Color(bitmap.pixels[index + 2],
+                    bitmap.pixels[index + 1],
+                    bitmap.pixels[index]);
+
+            for (var k in numberToColorDict)
+                if (numberToColorDict[k].equals(color))
+                    if (src === "input1") {
+                        mapInferior[i][j] = k;
+                        setFloorTexture(i, j, k);
+                    } else {
+                        mapSuperior[i][j] = k;
+                        setMapObject(i, j, k);
+                    }
+        }
+
+}
+
+function initializeDictionaries() {
+    var c = new Color(0, 0, 0);
+    var index = 0;
+    for (var i in c.colors()) {
+        numberToColorDict[index] = c.colors()[i];
+        numberToType[index++] = i;
+    }
+}
+function loadFloor() {
+    grassTexture = new THREE.ImageUtils.loadTexture('obj/floor/grass.jpg');
+    grassTexture.wrapS = grassTexture.wrapT = THREE.RepeatWrapping;
+    grassTexture.repeat.set(10, 10);
+    grassMaterial = new THREE.MeshBasicMaterial({map: grassTexture, side: THREE.DoubleSide});
+
+    waterTexture = new THREE.ImageUtils.loadTexture('obj/floor/water.jpg');
+    waterTexture.wrapS = grassTexture.wrapT = THREE.RepeatWrapping;
+    waterTexture.repeat.set(10, 10);
+    waterMaterial = new THREE.MeshBasicMaterial({map: waterTexture, side: THREE.DoubleSide});
+    floorGeometry = new THREE.PlaneGeometry(20, 20, 10, 10);
+}
+/**
+ * 
+ * @param {int} i linha
+ * @param {int} j coluna
+ * @param {string} k tipo do objeto lido no bitmap
+ * @returns {undefined}
+ */
+function setFloorTexture(i, j, k) {
+    //FLOOR
+    floor[i][j] = new THREE.Mesh(floorGeometry, (numberToType[k] === 'empty') ? waterMaterial : grassMaterial);
+    floor[i][j].position.x = i * mapScale;
+    floor[i][j].position.z = j * mapScale;
+    floor[i][j].position.y = -0.5;
+    floor[i][j].rotation.x = Math.PI / 2;
+    scene.add(floor[i][j]);
+    //
+}
+function setMapObject(i, j, k) {
+    console.log(mapInferior);
+    switch (numberToType[k]) {
+        case 'block':
+//            cannot be instanciated if water is below
+            if (numberToType[mapInferior[i][j]] === 'block') {
+                var b = new Block();
+                b.init(i * mapScale, 0, j * mapScale);
+                mapObjects[i][j] = b;
+            }
+            break;
+        default:
+            break;
+    }
+}
+/*----------------------MAP LOADING END-------------------------------------*/
+
+
 
 $('#document').ready(mainLoop);
--- javascripts/models.js
+++ javascripts/models.js
@@ -16,8 +16,10 @@ var Player = function() {
 	}
 
 
-	that.init = function() {
-
+	that.init = function(x,y,z) {
+            that.posx=x;
+            that.posy=y;
+            that.posz=z;
 		var manager = new THREE.LoadingManager();
 		manager.onProgress = function(item, loaded, total) {
 
@@ -62,8 +64,9 @@ var Player = function() {
 		});
 
 
-		object.position.x = posx;
-		object.position.y = posy;
+		object.position.x = that.posx;
+		object.position.y = that.posy;
+		object.position.z = that.posz;
                 var scale=0.05;
 		object.scale.x = scale;
 		object.scale.y = scale;
@@ -78,7 +81,7 @@ var Player = function() {
 
 
 
-	//Seters
+	//Setters
 	that.setPosx = function(new_posx) {
 		that.posx = new_posx;
 	};
@@ -105,7 +108,84 @@ var Player = function() {
 	that.getPlayerLoader = function() {
 		return that.player_loader;
 	};
+};
 
+var Block = function() {
+	var that = this;
+	var player_loader;
+	var block_object;
+	var texture = new THREE.Texture();
+	var posx = 0;
+	var posy = 0;
+	var posz = 0;
+	that.init = function(x,y,z) {
+            that.posx=x;
+            that.posy=y;
+            that.posz=z;
+		var manager = new THREE.LoadingManager();
+		manager.onProgress = function(item, loaded, total) {
 
+			console.log(item, loaded, total);
 
-}
+		};
+		var onProgress = function(xhr) {
+			if (xhr.lengthComputable) {
+				var percentComplete = xhr.loaded / xhr.total * 100;
+				console.log(Math.round(percentComplete, 2) + '% downloaded');
+			}
+		};
+		var onError = function(xhr) {};
+
+		var texture_loader = new THREE.ImageLoader(manager);
+		texture_loader.load('obj/lego/face_haryach.jpg', function(image) {
+			texture.image = image;
+			texture.needsUpdate = true;
+		});
+		// model
+		var loader = new THREE.OBJLoader(manager);
+		loader.load('obj/block/Wooden Box.obj', addObjtoScene);
+	};
+	function addObjtoScene(object) {
+		object.traverse(function(child) {
+			if (child instanceof THREE.Mesh) {
+				child.material.map = texture;
+			}
+		});
+
+		object.position.x = that.posx;
+		object.position.y = that.posy;
+		object.position.z = that.posz;
+                var scale=3;
+		object.scale.x = scale;
+		object.scale.y = scale;
+		object.scale.z = scale;
+		object.rotation.y = Math.PI;
+		object.rotation.x = 0;
+		that.block_object = object;
+		scene.add(that.block_object);
+	};
+	//Setters
+	that.setPosx = function(new_posx) {
+		that.posx = new_posx;
+	};
+	that.setPosy = function(new_posy) {
+		that.posy = new_posy;
+	};
+	that.setPosz = function(new_posz) {
+		that.posz = new_posz;
+	};
+	//getters
+	that.getPosx = function() {
+		return that.posx;
+	};
+	that.getPosy = function() {
+		return that.posy;
+	};
+	that.getPosz = function() {
+		return that.posz;
+	};
+
+	that.getPlayerLoader = function() {
+		return that.player_loader;
+	};
+};
